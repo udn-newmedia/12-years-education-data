b784b80e671a1d57fdaac024d0589becfd5ebb47
freddycro <freddy10100@hotmail.com>
2020-05-13 23:36:56 +0800

    change normalize.css to scss

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store';
 import { rwdMethods } from '@/mixins/masterBuilder.js';
-import 'normalize.css';
 import '~/style/main.scss';
 
 Vue.config.productionTip = false
